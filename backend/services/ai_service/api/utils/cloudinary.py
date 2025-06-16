490c6b38d59de8f889f9bebaded572777d6e8299
ndk <navnathkadam284@gmail.com>
2025-06-16 21:27:42 +0530

    fix, cloudinary upload

--- backend/services/ai_service/api/utils/cloudinary.py
+++ backend/services/ai_service/api/utils/cloudinary.py
@@ -13,7 +13,7 @@ async def upload_image_from_url(file_path, filename):
     cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"), 
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True
+    secure=True,
     )
 
     try:
